3b3fe96589c318319be6e55210a690b34adc2a29
niklasjansson-truesec <niklas.jansson@truesec.se>
2025-11-21 10:34:58 +0100

    Update main.tf

--- modules/module-1/main.tf
+++ modules/module-1/main.tf
@@ -3445,7 +3445,7 @@ resource "aws_internet_gateway" "goat_gw" {
 resource "aws_subnet" "goat_subnet" {
   vpc_id                  = aws_vpc.goat_vpc.id
   cidr_block              = "192.168.0.0/24"
-  availability_zone       = "eu-north-1"
+  availability_zone       = "eu-north-1a"
   map_public_ip_on_launch = true
   tags = {
     Name = "AWS_GOAT App subnet"
